0bab126a25ebd8ef4a33322d5a341d622f16bd29
rellort1 <rellorttf2@gmail.com>
2026-05-29 22:16:50 -0500

    fix ilresetrc
    update script with new socket

--- ilresetrc.sh
+++ ilresetrc.sh
@@ -3,6 +3,6 @@ restart="19:1 19:0"
 up="103:1 103:0"
 confirm="28:1 28:0"
 
-YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool key -d 30 $restart $up $confirm
+ydotool key -d 30 $restart $up $confirm
 sleep 1.4
-YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool key -d 30 59:1 59:0
+ydotool key -d 30 59:1 59:0
